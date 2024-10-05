93c9ce8358f06a37e6d2cbf0eae9b57aa34df69a
zellchristensen <38737447+zellchristensen@users.noreply.github.com>
2024-10-05 02:10:29 -0500

    Create RedditTextSpoilers.js
    Automatically clicks all <shreddit-spoiler> elements on the page after it loads.

---
+++ RedditTextSpoilers.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Auto Click Shreddit Spoilers (Reddit Text Spoiler Clicker)
+// @namespace    https://raw.githubusercontent.com/zellchristensen/UserScripts/main/RedditTextSpoilers.js
+// @downloadURL  https://raw.githubusercontent.com/zellchristensen/UserScripts/main/RedditTextSpoilers.js
+// @version      2024-10-05_1.0
+// @description  Automatically clicks all <shreddit-spoiler> elements on the page after it loads.
+// @author       Zell
+// @match        https://www.reddit.com/*
+// @icon         https://www.redditstatic.com/shreddit/assets/favicon/192x192.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+}
